ed58271b7ddb71a4a5e292c31d0730ec5e284df4
float <paddingme@163.com>
2022-04-25 16:07:33 +0800

    跨域问题

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type")
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("Cache-Control", "no-store")//304
-  if (req.method === "OPTIONS") res.send(200)
-  else next()
+  next()
 });
 
 // router
