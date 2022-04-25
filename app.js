7098c308e41afd97780ff91caf785affe5a80e16
float <paddingme@163.com>
2022-04-25 15:48:52 +0800

    跨域调整

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ app.use(cookieParser())
 
 // 跨域
 app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", req.headers.origin)
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type")
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
   res.header("Cache-Control", "no-store")//304
-  next()
+  if (req.method === "OPTIONS") res.send(200)
+  else next()
 });
 
 // router
