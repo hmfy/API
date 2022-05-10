1f47c91ce209013f9eb251185d83045177c1c0e1
float <paddingme@163.com>
2022-05-10 16:33:01 +0800

    '账号密码判断逻辑'

--- routes/user.js
+++ routes/user.js
@@ -9,20 +9,22 @@ router.post('/login', async (req, res) => {
 	let { username, password } = req.body
 	username = getDecrypt(username)
 	password = getDecrypt(password)
-	const [ { password: truePaw, ID } ] = await knex.select('password', 'ID').from('User').where('user', username)
-	if (truePaw === password) {
-		// 密码正确
-		res.send({
-			err: null,
-			msg: '登陆成功！',
-			token: setToken(ID)
-		})
-	} else {
-		// 密码错误
-		res.send({
-			err: '密码或密码有误！'
-		})
+	const result = await knex.select('password', 'ID').from('User').where('user', username)
+	if (result.length) {
+		const { password: truePaw, ID } = result[0]
+		if (truePaw === password) {
+			// 密码正确
+			return res.send({
+				err: null,
+				msg: '登陆成功！',
+				token: setToken(ID)
+			})
+		}
 	}
+	// 密码错误
+	res.send({
+		err: '密码或密码有误！'
+	})
 })
 
 module.exports = router
